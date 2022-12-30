1cbfa97e37410aa5a287b9e053e701d1bcecbeed
huy.td1 <huy.td1@samsung.com>
2022-12-30 10:50:36 +0700

    [CS-12192201] Modify version history view concept for tool manager tab, show it on a separate window

---
+++ cyber_server/views/extension/ShowVersionHistoryButton.cs
@@ -0,0 +1,60 @@
+﻿using cyber_server.views.windows.others;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace cyber_server.views.extension
+{
+    internal class ShowVersionHistoryButton : ToggleButton
+    {
+        private VersionHistoryWindow _windowCache;
+
+        #region VersionHistorySource
+        public static readonly DependencyProperty VersionHistorySourceProperty =
+            DependencyProperty.Register(
+                "VersionHistorySource",
+                typeof(IEnumerable),
+                typeof(ShowVersionHistoryButton),
+                new PropertyMetadata(default(IEnumerable), new PropertyChangedCallback(OnVersionHistorySourceChangedCallback)));
+
+        private static void OnVersionHistorySourceChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var ctrl = d as ShowVersionHistoryButton;
+            ctrl?._windowCache?.SetVersionHistorySource((IEnumerable)e.NewValue);
+        }
+
+        public IEnumerable VersionHistorySource
+        {
+            get => (IEnumerable)GetValue(VersionHistorySourceProperty);
+            set => SetValue(VersionHistorySourceProperty, value);
+        }
+        #endregion
+
+        protected override void OnClick()
+        {
+            base.OnClick();
+            var isShouldOpenVersionHistoryWindow = IsChecked ?? false;
+            if (isShouldOpenVersionHistoryWindow)
+            {
+                var vhw = new VersionHistoryWindow(VersionHistorySource);
+                vhw.Closed += (s, arg) =>
+                {
+                    IsChecked = false;
+                };
+                vhw.Owner = App.Current.ServerWindow;
+                vhw.WindowStartupLocation = WindowStartupLocation.CenterOwner;
+                _windowCache = vhw;
+                _windowCache.Show();
+            }
+            else
+            {
+                _windowCache.Close();
+            }
+        }
+    }
+}
---
+++ cyber_server/views/windows/others/VersionHistoryWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace cyber_server.views.windows.others
+{
+
+
+
+    /// <summary>
+    /// Interaction logic for VersionHistoryWindow.xaml
+    /// </summary>
+    public partial class VersionHistoryWindow : Window
+    {
+        public VersionHistoryWindow(IEnumerable versionHistorySource)
+        {
+            InitializeComponent();
+            PART_VersionHistoryListView.ItemsSource = versionHistorySource;
+        }
+
+        public void SetVersionHistorySource(IEnumerable source)
+        {
+            PART_VersionHistoryListView.ItemsSource = null;
+            PART_VersionHistoryListView.ItemsSource = source;
+        }
+    }
+}
